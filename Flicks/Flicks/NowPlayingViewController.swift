40107bd37b1cb9aac5e93c3e5ec1f327fc98a450
Anup Kher <anup.kher.1990@gmail.com>
2017-03-31 14:59:41 -0700

    Creating xcode project with now playing screen

---
+++ Flicks/Flicks/NowPlayingViewController.swift
@@ -0,0 +1,52 @@
+//
+//  NowPlayingViewController.swift
+//  Flicks
+//
+//  Created by Anup Kher on 3/31/17.
+//  Copyright © 2017 codepath. All rights reserved.
+//
+
+import UIKit
+
+class NowPlayingViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    @IBOutlet weak var nowPlayingTableView: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        nowPlayingTableView.delegate = self
+        nowPlayingTableView.dataSource = self
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return MovieTableViewCell()
+    }
+    
+    func getApiConfiguration() {
+        
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
