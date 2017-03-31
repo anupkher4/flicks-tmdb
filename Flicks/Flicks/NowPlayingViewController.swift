ee165e26ce777cf18002836ace63a709fb537306
Anup Kher <anup.kher.1990@gmail.com>
2017-03-31 16:25:44 -0700

    Adding now playing api

--- Flicks/Flicks/NowPlayingViewController.swift
+++ Flicks/Flicks/NowPlayingViewController.swift
@@ -7,15 +7,24 @@
 //
 
 import UIKit
+import AFNetworking
 
 class NowPlayingViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     @IBOutlet weak var nowPlayingTableView: UITableView!
 
+    var posterBaseUrl = ""
+    var posterSize = ""
+    var movies: [NSDictionary] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         nowPlayingTableView.delegate = self
         nowPlayingTableView.dataSource = self
+        nowPlayingTableView.rowHeight = 120
+        
+        getApiConfiguration()
+        getNowPlayingMovies()
     }
 
     override func didReceiveMemoryWarning() {
@@ -28,15 +37,81 @@ class NowPlayingViewController: UIViewController, UITableViewDelegate, UITableVi
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return movies.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return MovieTableViewCell()
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath) as! MovieTableViewCell
+        
+        let movie = movies[indexPath.row]
+        
+        if let posterPath = movie.value(forKeyPath: "poster_path") as? String {
+            if let image = getMoviePosterImage(baseUrl: posterBaseUrl, size: posterSize, posterPath: posterPath) {
+                cell.posterImageView.image = image
+            }
+        }
+        if let title = movie.value(forKeyPath: "original_title") as? String {
+            cell.titleLabel.text = title
+        }
+        if let overview = movie.value(forKeyPath: "overview") as? String {
+            cell.overviewLabel.text = overview
+        }
+        
+        return cell
     }
     
     func getApiConfiguration() {
+        let url = URL(string: "https://api.themoviedb.org/3/configuration?api_key=0bae87a1c2bc3fd65e17a82fec52d5c7")
+        let request = URLRequest(url: url!)
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
         
+        let task: URLSessionDataTask = session.dataTask(with: request) {
+            (data, response, error) in
+            if let data = data {
+                if let responseDictionary = try? JSONSerialization.jsonObject(with: data, options: []) as? NSDictionary {
+                    if let images = responseDictionary?.value(forKeyPath: "images") as? NSDictionary {
+                        if let baseUrl = images.value(forKeyPath: "secure_base_url") as? String {
+                            self.posterBaseUrl = baseUrl
+                        }
+                        if let posterSizes = images.value(forKeyPath: "poster_sizes") as? [String] {
+                            self.posterSize = posterSizes[0]
+                        }
+                    }
+                }
+            }
+        }
+        task.resume()
+    }
+    
+    func getNowPlayingMovies() {
+        let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=0bae87a1c2bc3fd65e17a82fec52d5c7")
+        let request = URLRequest(url: url!)
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        
+        let task: URLSessionDataTask = session.dataTask(with: request) {
+            (data, response, error) in
+            if let data = data {
+                if let responseDictionary = try? JSONSerialization.jsonObject(with: data, options: []) as? NSDictionary {
+                    if let results = responseDictionary?.value(forKeyPath: "results") as? [NSDictionary] {
+                        self.movies = results
+                        self.nowPlayingTableView.reloadData()
+                    }
+                }
+            }
+        }
+        task.resume()
+    }
+    
+    func getMoviePosterImage(baseUrl: String, size: String, posterPath: String) -> UIImage? {
+        let urlString = "\(baseUrl)\(size)/\(posterPath)"
+        let url = URL(string: urlString)
+        if let data = try? Data(contentsOf: url!) {
+            if let image = UIImage(data: data) {
+                return image
+            }
+        }
+        
+        return nil
     }
 
     /*
