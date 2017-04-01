49d351359024dcb83f7ebe0643c66d045760c2ed
Anup Kher <anup.kher.1990@gmail.com>
2017-03-31 21:08:44 -0700

    Populating movie details

---
+++ Flicks/Flicks/MovieDetailsViewController.swift
@@ -0,0 +1,156 @@
+//
+//  MovieDetailsViewController.swift
+//  Flicks
+//
+//  Created by Anup Kher on 3/31/17.
+//  Copyright © 2017 codepath. All rights reserved.
+//
+
+import UIKit
+
+class MovieDetailsViewController: UIViewController {
+    @IBOutlet weak var bigPosterImageView: UIImageView!
+    @IBOutlet weak var movieDetailsView: UIView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var releaseDateLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var runningTimeLabel: UILabel!
+    @IBOutlet weak var overviewLabel: UILabel!
+    
+    let defaults = UserDefaults.standard
+    
+    var movieId: Int!
+    var movie: NSDictionary = [:]
+    var posterBaseUrl = ""
+    var posterSize = ""
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        if let baseUrl = defaults.object(forKey: "base_url") as? String {
+            posterBaseUrl = baseUrl
+        }
+        if let posterSizes = defaults.object(forKey: "poster_sizes") as? [String] {
+            posterSize = posterSizes[4]
+        }
+        
+        getMovieDetails(id: movieId)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    // MARK: - TMDB API Helper Methods
+    
+    func getMovieDetails(id: Int) {
+        let urlString = "https://api.themoviedb.org/3/movie/\(id)?api_key=0bae87a1c2bc3fd65e17a82fec52d5c7&language=en-US"
+        let url = URL(string: urlString)
+        let request = URLRequest(url: url!)
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        
+        let task: URLSessionDataTask = session.dataTask(with: request) {
+            (data, response, error) in
+            if let data = data {
+                if let movie = try! JSONSerialization.jsonObject(with: data, options: []) as? NSDictionary {
+                    if let posterPath = movie.value(forKeyPath: "poster_path") as? String {
+                        if let image = self.getMoviePosterImage(baseUrl: self.posterBaseUrl, size: self.posterSize, posterPath: posterPath) {
+                            self.bigPosterImageView.image = image
+                        }
+                    }
+                    if let title = movie.value(forKeyPath: "original_title") as? String {
+                        self.titleLabel.text = title
+                    }
+                    if let release = movie.value(forKeyPath: "release_date") as? String {
+                        self.releaseDateLabel.text = self.getFormattedDate(string: release)
+                    }
+                    if let score = movie.value(forKeyPath: "vote_average") as? Double {
+                        let percentageScore = Int(exactly: (score / 10.0) * 100)
+                        self.scoreLabel.text = "\(percentageScore!)%"
+                    }
+                    if let running = movie.value(forKeyPath: "runtime") as? Int {
+                        let hours = running / 60
+                        let minutes = running % 60
+                        self.runningTimeLabel.text = "\(hours) hr \(minutes) mins"
+                    }
+                    if let overview = movie.value(forKeyPath: "overview") as? String {
+                        self.overviewLabel.text = overview
+                    }
+                }
+            }
+        }
+        task.resume()
+    }
+    
+    func getMoviePosterImage(baseUrl: String, size: String, posterPath: String) -> UIImage? {
+        let urlString = "\(baseUrl)\(size)/\(posterPath)"
+        let url = URL(string: urlString)
+        if let data = try? Data(contentsOf: url!) {
+            if let image = UIImage(data: data) {
+                return image
+            }
+        }
+        
+        return nil
+    }
+    
+    func getFormattedDate(string: String) -> String {
+        var formattedDate = ""
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd"
+        let calendar = Calendar(identifier: .gregorian)
+        
+        if let date = dateFormatter.date(from: string) {
+            let components = calendar.dateComponents([.day, .month, .year], from: date)
+            if let day = components.day, let month = components.month, let year = components.year {
+                var fullMonth = ""
+                switch month {
+                case 1:
+                    fullMonth = "January"
+                case 2:
+                    fullMonth = "February"
+                case 3:
+                    fullMonth = "March"
+                case 4:
+                    fullMonth = "April"
+                case 5:
+                    fullMonth = "May"
+                case 6:
+                    fullMonth = "June"
+                case 7:
+                    fullMonth = "July"
+                case 8:
+                    fullMonth = "August"
+                case 9:
+                    fullMonth = "September"
+                case 10:
+                    fullMonth = "October"
+                case 11:
+                    fullMonth = "November"
+                case 12:
+                    fullMonth = "December"
+                default:
+                    fullMonth = ""
+                }
+                
+                formattedDate = "\(fullMonth) \(day), \(year)"
+            }
+        }
+        
+        return formattedDate
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Flicks/Flicks/NowPlayingViewController.swift
+++ Flicks/Flicks/NowPlayingViewController.swift
@@ -12,8 +12,11 @@ import AFNetworking
 class NowPlayingViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     @IBOutlet weak var nowPlayingTableView: UITableView!
 
+    let defaults = UserDefaults.standard
+    
     var posterBaseUrl = ""
     var posterSize = ""
+    var bigPosterSize = ""
     var movies: [NSDictionary] = []
     
     override func viewDidLoad() {
@@ -31,6 +34,8 @@ class NowPlayingViewController: UIViewController, UITableViewDelegate, UITableVi
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    // MARK: - Table View Delegate Methods
     
     func numberOfSections(in tableView: UITableView) -> Int {
         return 1
@@ -60,6 +65,12 @@ class NowPlayingViewController: UIViewController, UITableViewDelegate, UITableVi
         return cell
     }
     
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    // MARK: - TMDB API Helper Methods
+    
     func getApiConfiguration() {
         let url = URL(string: "https://api.themoviedb.org/3/configuration?api_key=0bae87a1c2bc3fd65e17a82fec52d5c7")
         let request = URLRequest(url: url!)
@@ -72,9 +83,16 @@ class NowPlayingViewController: UIViewController, UITableViewDelegate, UITableVi
                     if let images = responseDictionary?.value(forKeyPath: "images") as? NSDictionary {
                         if let baseUrl = images.value(forKeyPath: "secure_base_url") as? String {
                             self.posterBaseUrl = baseUrl
+                            if self.defaults.object(forKey: "base_url") == nil {
+                                self.defaults.set(baseUrl, forKey: "base_url")
+                            }
                         }
                         if let posterSizes = images.value(forKeyPath: "poster_sizes") as? [String] {
                             self.posterSize = posterSizes[0]
+                            self.bigPosterSize = posterSizes[4]
+                            if self.defaults.object(forKey: "poster_sizes") == nil {
+                                self.defaults.set(posterSizes, forKey: "poster_sizes")
+                            }
                         }
                     }
                 }
@@ -114,14 +132,21 @@ class NowPlayingViewController: UIViewController, UITableViewDelegate, UITableVi
         return nil
     }
 
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        if segue.identifier == "movieToDetails" {
+            let destinationVc = segue.destination as! MovieDetailsViewController
+            let indexPath = nowPlayingTableView.indexPath(for: sender as! MovieTableViewCell)
+            let selectedMovie = movies[indexPath!.row]
+            let id = selectedMovie.value(forKeyPath: "id") as! Int
+            
+            destinationVc.movieId = id
+            //print(id)
+        }
     }
-    */
+    
 
 }
